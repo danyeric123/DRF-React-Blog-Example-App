d857559c1bf39d0a5d8b7c7caea783764edaadfe
danyeric123 <dnagarpowers@gmail.com>
2021-09-19 10:32:17 -0400

    post details changes and better date formatting

--- blog-frontend/src/components/PostCard.tsx
+++ blog-frontend/src/components/PostCard.tsx
@@ -1,4 +1,6 @@
+import moment from 'moment'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 interface Snippet {
   title: string;
@@ -15,12 +17,18 @@ const PostCard : React.FC <Snippet> = ({title,author,excerpt,status,published,ca
     <div
       className="post-card"
     >
+      <Link
+        to={{
+          pathname: `/posts/${title}`
+        }}
+      >
       <h1>{title}</h1>
       <h3>{status}</h3>
-      <small>{published}</small>
+      <h4>{moment(published).format('MMMM Do YYYY, h:mm:ss a')}</h4>
       <small>Written by {author}</small>
       <p>{excerpt}</p>
       <small>Categories: {category}</small>
+      </Link>
 
     </div>
   )
--- blog-frontend/src/pages/PostDetails.tsx
+++ blog-frontend/src/pages/PostDetails.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { getPost, Post } from '../services/blogServices'
+import moment from 'moment'
 
 type params = {
   title: string
@@ -22,8 +23,8 @@ const PostDetails = () => {
       <h1>{post?.title}</h1>
       <small>By {post?.author}</small>
       <p>{post?.content}</p>
-      <small>Publication Date: {post?.published}</small>
-      <small>Categories: {post?.category.map(cat=>cat+",")}</small>
+      <small>Publication Date: {moment(post?.published).format('MMMM Do YYYY, h:mm:ss a')}</small>
+      {post?.category&& <small>Categories: {post?.category.map(cat=>cat+",")}</small> }
     </>
   )
 }
